5249a2971ebca94a1d0f793c8699e1aa7e9bcbcc
Brian Carroll <brian.carroll.ireland@gmail.com>
2016-09-27 19:24:33 +0100

    Display event names and images

--- src/View.elm
+++ src/View.elm
@@ -1,20 +1,49 @@
 module View exposing (root)
 
 -- External modules
-import Html exposing (Html, div, text, img)
-import Html.Attributes exposing (src, style)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Maybe
 
 -- Local modules
 import Types exposing (..)
-
+import TicketMaster
 
 
 root : Model -> Html Msg
 root model =
     div []
-        [ img [ src "/assets/fb-profile-pic.jpg"
-              , style [("height", "100px")]
-              ]
-              []
-        , text (toString model)
+        [ tmdata model.data
         ]
+
+tmdata : WebData { a | events : List TicketMaster.Event } -> Html Msg
+tmdata webdata =
+    case webdata of
+        NotAsked ->
+            text "Request not sent yet"
+        Loading ->
+            text "Loading..."
+        Failure error ->
+            text (toString error)
+        Success data ->
+            div []
+                (List.map tmevent data.events)
+
+
+tmevent : TicketMaster.Event -> Html Msg
+tmevent event =
+    div []
+        [ a [ href event.url ]
+            [ text event.name
+            ]
+        , img [src (firstEventImageUrl event)] []
+        ]
+
+firstEventImageUrl : TicketMaster.Event -> String
+firstEventImageUrl event =
+    case List.head event.images of
+        Nothing ->
+            ""
+
+        Just image ->
+            image.url
