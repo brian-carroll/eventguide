2f831ac86dd3cf6a520bbabc2627292ee43d2be6
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-03-19 02:54:39 +0000

    Styling

--- src/Styles/Card.elm
+++ src/Styles/Card.elm
@@ -2,6 +2,7 @@ module Styles.Card exposing (css)
 
 import Css exposing (..)
 import Styles.Selectors exposing (..)
+import Styles.Colours exposing (white, greyL, greyM, asphaltL, asphaltM)
 
 
 css : Stylesheet
@@ -31,132 +32,121 @@ css =
                     ]
                 ]
             ]
+        , class Card
+            [ verticalAlign top
+            , width (pct 100)
+            , borderRadius (px 2)
+            , overflow hidden
+              -- , backfaceVisibility hidden
+            , textRendering optimizeLegibility
+            , transform <| translateZ zero
+              -- , hover
+              --     [ BgOverlay
+              --         [ opacity 0
+              --         ]
+              --     ]
+            ]
+          -- , class BgOverlay
+          --     [ zIndex 1
+          --     ]
+          -- , class BtnPlay
+          --     [ position absolute
+          --     , top (pct 50)
+          --     , left (pct 50)
+          --     , backgroundColor asphaltM
+          --       -- transform translate((pct -50),(pct -50)) scale(0.8),
+          --     ]
+          -- , class CardEmpty
+          --     [ marginTop (px 32)
+          --     , marginBottom (px 32)
+          --     , descendants
+          --         [ class CardCover
+          --             [ backgroundColor transparent
+          --             , border (px 3) dashed greyL
+          --             , borderRadius (px 2)
+          --             , transition all 0.3 s cubic
+          --             ]
+          --         , class CardCta
+          --             [ position absolute
+          --             , bottom 0
+          --             , margin 0
+          --             , width (pct 100)
+          --             , padding (px 12)
+          --             , color brandprimary
+          --             , textAlign center
+          --             , textTransform uppercase
+          --             , fontSize (Css.rem 0.9)
+          --             ]
+          --         , class BtnPlay
+          --             [ fontSize (px 60)
+          --             , lineHeight (px 95)
+          --             , backgroundColor transparent
+          --             , borderColor greyL
+          --             , color greyL
+          --             ]
+          --         , hover
+          --             [ CardCover
+          --                 [ borderColor asphaltL
+          --                 ]
+          --             ]
+          --         ]
+          --     ]
+          -- , class CardCover
+          --     [ position relative
+          --     , backgroundColor greyL
+          --     , backgroundSize cover
+          --     , backgroundPosition center center
+          --     , backgroundRepeat noRepeat
+          --       -- , backfaceVisibility hidden
+          --     , overflow hidden
+          --     , after
+          --         [ display block
+          --         , content ""
+          --         , width (pct 100)
+          --         , paddingTop (px 56) (pct 25)
+          --         ]
+          --     , class Badge
+          --         [ position absolute
+          --         , zIndex 5
+          --         , top (px 16)
+          --         , right (px 24)
+          --         , borderColor white
+          --         , backgroundColor asphaltM
+          --         , color white
+          --           -- , backfaceVisibility hidden
+          --         ]
+          --     ]
+          -- , class CardPrice
+          --     [ display block
+          --     , position absolute
+          --     , zIndex 6
+          --     , bottom (px 16)
+          --     , right (px 24)
+          --     , padding (px 4) (px 12)
+          --     , border (px 1) solid white
+          --     , borderRadius (px 30)
+          --     , backgroundColor asphaltM
+          --     , color white
+          --       -- , backfaceVisibility hidden
+          --     ]
+          -- , class CardHeading
+          --     [ margin (Css.rem 0.5) 0
+          --     , fontWeight 700
+          --     ]
+          -- , class CardSubHeading
+          --     [ marginBottom 0
+          --     , fontFamily bodyfont
+          --     , fontWeight 400
+          --     , fontSize (Css.rem 0.9)
+          --     , color greyM
+          --     , textTransform capitalize
+          --     ]
+        , class CardImage
+            [ position absolute
+            , top zero
+            , left zero
+            , right zero
+            , bottom zero
+            , width (pct 100)
+            ]
         ]
-
-
-
-{-
-   .card {
-     vertical-align: top;
-     width: 100%;
-     border-radius: 2px;
-     overflow: hidden;
-     backface-visibility: hidden;
-     text-rendering: optimizeLegibility;
-     transform: translateZ(0);
-     &:hover {
-       .bg-overlay {
-         opacity: 0;
-       }
-     }
-     .bg-overlay {
-       z-index: 1;
-     }
-     .btn--play {
-       position: absolute;
-       top: 50%;
-       left: 50%;
-       background-color: rgba($asphalt-m,0.5);
-       transform: translate(-50%,-50%) scale(0.8);
-     }
-   }
-
-   .card.card--empty {
-     margin-top: 32px;
-     margin-bottom: 32px;
-     .card__cover {
-       background-color: transparent;
-       border: 3px dashed $grey-l;
-       border-radius: 2px;
-       transition: all 0.3s $cubic;
-     }
-     .card__cta {
-       position: absolute;
-       bottom: 0;
-       margin: 0;
-       width: 100%;
-       padding: 12px;
-       color: $brand-primary;
-       text-align: center;
-       text-transform: uppercase;
-       font-size: 0.9rem;
-     }
-     .btn--play {
-       font-size: 60px;
-       line-height: 95px;
-       background-color: transparent;
-       border-color: $grey-l;
-       color: $grey-l;
-     }
-     &:hover {
-       .card__cover {
-         border-color: rgba($asphalt-l,0.75);
-       }
-     }
-   }
-
-   .card__cover {
-     position: relative;
-     background-color: $grey-l;
-     background-size: cover;
-     background-position: center center;
-     background-repeat: no-repeat;
-     backface-visibility: hidden;
-     overflow: hidden;
-     //border-bottom: 1px solid $grey-l;
-     &:after {
-       display: block;
-       content: "";
-       width: 100%;
-       padding-top: 56.25%;
-     }
-     .badge {
-       position: absolute;
-       z-index: 5;
-       top: 16px;
-       right: 24px;
-       border-color: $white;
-       background-color: rgba($asphalt-m,0.5);
-       color: $white;
-       backface-visibility: hidden;
-     }
-   }
-
-   .card__price {
-     display: block;
-     position: absolute;
-     z-index: 6;
-     bottom: 16px;
-     right: 24px;
-     padding: 4px 12px;
-     border: 1px solid $white;
-     border-radius: 30px;
-     background-color: rgba($asphalt-m,0.5);
-     color: $white;
-     backface-visibility: hidden;
-   }
-
-   .card__heading {
-     margin: 0.5rem 0;
-     font-weight: 700;
-   }
-
-   .card__sub-heading {
-     margin-bottom: 0;
-     font-family: $body-font;
-     font-weight: 400;
-     font-size: 0.9rem;
-     color: $grey-m;
-     text-transform: capitalize;
-   }
-
-   .card__image {
-     position: absolute;
-     top: 0;
-     left: 0;
-     right: 0;
-     bottom: 0;
-     width: 100%;
-   }
-
--}
---
+++ src/Styles/Colours.elm
@@ -0,0 +1,63 @@
+module Styles.Colours exposing (..)
+
+import Css exposing (hex)
+
+
+black =
+    hex "262730"
+
+
+white =
+    hex "FCFCFC"
+
+
+greyXl =
+    hex "f6f6f6"
+
+
+greyL =
+    hex "dbdbdb"
+
+
+greyM =
+    hex "999999"
+
+
+greyD =
+    hex "686868"
+
+
+brandPrimary =
+    hex "F47E5A"
+
+
+brandPrimaryL =
+    hex "F3B379"
+
+
+brandSecondary =
+    hex "59C3C3"
+
+
+asphaltL =
+    hex "61707D"
+
+
+asphaltM =
+    hex "424C55"
+
+
+blue =
+    hex "279AF1"
+
+
+green =
+    hex "23B53D"
+
+
+red =
+    hex "DB5461"
+
+
+yellow =
+    hex "FFD84F"
